f028d721640b11819e6e544d04396aae7542e83f
MudusarithaGangamina <gangaminakumara@gmail.com>
2024-12-29 23:10:58 +0530

    Setting Up Tailwind CSS and Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,8 @@ import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Home</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
